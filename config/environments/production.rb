ae6478e7ec064c89fde2475daa5fc056105545ae
Le Hoang Tam <tamb1304723@gmail.com>
2018-02-26 21:46:24 +0700

    hẻoku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,12 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -80,6 +85,7 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
+
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
